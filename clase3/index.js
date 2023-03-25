b1eb36208a86a66df0f1f832c282e82d155689c6
BioDani <dtejadah@unal.edu.co>
2023-03-25 10:42:44 -0500

    uso de express para usar get y post

--- clase3/index.js
+++ clase3/index.js
@@ -1,10 +1,59 @@
 const express = require("express");  // importar express
 const PORT = 3000; // definición del puerto
 const app = express(); // acá definiremos las rutas
+app.use(express.json) //¿para qué era?
 
-app.get("/", () => {
-    resizeBy.send(`esta es mi primera app en express`);
+
+const errorlogger = () => {};
+const errorHandler = () => {};
+
+
+let products = [
+    {
+        id : 1, 
+        name : `reloj`,
+        price : 2000,
+        quantity : 10
+    },
+    {
+        id : 2, 
+        name : `reloj2`,
+        price : 3000,
+        quantity : 12
+    },
+    {
+        id : 3, 
+        name : `reloj3`,
+        price : 4000,
+        quantity : 34
+    }
+];
+
+app.get("/", (req, res) => {
+    res.send(`Primera app de express`);
 });
+
+// Trae todos los productos
+app.get("/api/v1/products", (req, res) => {
+    res.json(products);
+    console.log(`funciona`);
+});
+
+// Trae un producto por id
+app.get("/api/v1/products/:productId", (req, res) => {
+    console.log(req.params);
+    const {productId} = req.params; // Desestructuramos el objeto que trae 
+    console.log(productId);
+
+    const productIdInt = parseInt(productId);
+    const product = products.find((product)=> product.id === productIdInt);
+    res.json(product);
+});
+
+app.post('/api/v1/products',(req,res) =>{
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Escuchando en el http://localhost:${PORT}`);

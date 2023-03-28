ff7753bbbc14d1cae6c8f1aafb46fc46d10da4b2
BioDani <dtejadah@unal.edu.co>
2023-03-27 23:32:22 -0500

    detalles corregidos

--- fundamentos_node/10_fileSystem/10_fileSystem.js
+++ fundamentos_node/10_fileSystem/10_fileSystem.js
@@ -1,6 +1,7 @@
 //Modulo File System
 // Permite acceder a archivos leerlos, escribirlos, modificarlos, cambiar nombres, entre otras.
 
+const { triggerAsyncId } = require("async_hooks");
 const fs = require("fs/promises"); // importar file system
 
 // 1. Leer un archivo: Puede ser un proceso largo, idealmente debería hacerse asíncrona.
@@ -14,9 +15,33 @@ async function leerArchivo(path){
     }
 };
 
+// 2. Escribir un nuevo archivo
+async function escribirArchivo(path, contenido) {
+    try {
+        await fs.writeFile(path, contenido);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// 3. Borrar un archivo
+async function borrarArchivo(path) {
+    try {
+        await fs.unlink(path);
+    } catch (error) {
+        console.error('No se pudo borrar el archivo');
+    }
+}
+
 async function main() {
     const data =  await leerArchivo(`${__dirname}/10_archivo.txt`);
     console.log(data);
+
+    let texto = `Este es un nuevo archivo V02`;
+    const nombreArchivo = `10_archivoNuevo.txt`;
+    await escribirArchivo(`${__dirname}/${nombreArchivo}`, texto);
 };
 
 main();
+borrarArchivo(`${__dirname}/archivo.txt`);
+//borrarArchivo(`${__dirname}/10_archivoNuevo.txt`);

34a1b4858ad16e685d8753c1077069b2dc1eadf9
BioDani <dtejadah@unal.edu.co>
2023-04-07 00:23:34 -0500

    get, post, put, patch and delete working

--- API_rest_express/routes/products.router.js
+++ API_rest_express/routes/products.router.js
@@ -9,9 +9,16 @@ const service = new ProductService;
 // 1. Show all products
 // /api/v1/products?limit=10&offset=10
 router.get('/', (req, res) =>{ // request & response
-  const {limit} = req.query;
+  const {size} = req.query;
   const products = service.find();
   res.json(products);
+});
+
+// 5. Show a specific product
+router.get('/:id', (req, res) =>{ // request & response
+  const { id } = req.params;
+  const product = service.findOne(id);
+  res.json(product)
 });
 
 // 2. Create a new product
@@ -25,30 +32,26 @@ router.post('/', (req,res) => {
 router.patch('/:id', (req,res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message : `Updated partially the product id:${id}`,
-    data : body,
-    id,
-  })
+  const product = service.update(id,body);
+  res.json(product)
 });
 
-// 4. Update completely a product
+// 3. Update partially a product
 router.put('/:id', (req,res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message : `Updated the product id:${id}`,
-    data : body,
-    id,
-  });
+  const product = service.update(id,body);
+  res.json(product)
 });
 
-// 5. Get a specific product
-router.get('/:id', (req, res) =>{ // request & response
+
+// 4. Delete  a product
+router.delete('/:id', (req,res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
-  res.json(product);
+  const product = service.delete(id);
+  res.status(201).json(product)
 });
+
 
 // Exports
 module.exports = router;
--- API_rest_express/services/product.service.js
+++ API_rest_express/services/product.service.js
@@ -9,14 +9,14 @@ class ProductService {
     this.generate(); // Each time an product service's instance is created, generate() is runned
   };
 
-  generate(limit){  // Method of the class
-    for (let index = 0; index < (limit||10); index++) {
+  generate(){  // Method of the class
+    for (let index = 0; index < (4); index++) {
       this.products.push(
         {
           id : index,
           name : faker.commerce.productName(),
           quantity : parseInt(faker.random.numeric(2)),
-          price : faker.commerce.price(),
+          price : parseInt(faker.commerce.price()),
           image : faker.image.imageUrl()
         }
       )
@@ -24,8 +24,10 @@ class ProductService {
   };
 
   create(data){ // Method of the class
+    //const lastId = this.products.slice(-1)[0].id;
+    const lastId = this.products[this.products.length-1].id;
     const newProduct = {
-      id : ++this.products.slice(-1)[0].id , // slice return a list
+      id : lastId +1, // slice return a list
       ...data
     };
     this.products.push(newProduct);
@@ -40,13 +42,30 @@ class ProductService {
     return this.products.find(product => product.id === parseInt(id));
   }
 
-  update(){ // Method of the class
+  update(id, changes){ // Method of the class
+    const index = this.products.findIndex(product => product.id === parseInt(id));
+    if (index === -1){
+      throw new Error(`Product not found`);
+    } else {
+      const product = this.products[index];
+      this.products[index] = {
+        ...product,
+        ...changes
+      }
+    };
+    return this.products[index];
+  };
 
-  }
+  delete(id){ // Method of the class
+    const index = this.products.findIndex(product => product.id === parseInt(id));
+    if (index === -1){
+      throw new Error(`Product not found`);
+    }else{
+      this.products.splice(index,1);
+      return this.products[index];
+    }
+  };
 
-  delete(){ // Method of the class
-
-  }
 };
 
 // Exports

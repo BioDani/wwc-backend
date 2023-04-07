6fae37c490d0c907bbb97aff7fc4b7319bc15303
BioDani <dtejadah@unal.edu.co>
2023-04-07 00:28:17 -0500

    coments

--- API_rest_express/routes/products.router.js
+++ API_rest_express/routes/products.router.js
@@ -7,28 +7,27 @@ const router = express.Router()
 const service = new ProductService;
 
 // 1. Show all products
-// /api/v1/products?limit=10&offset=10
 router.get('/', (req, res) =>{ // request & response
   const {size} = req.query;
   const products = service.find();
   res.json(products);
 });
 
-// 5. Show a specific product
+// 2. Show a specific product
 router.get('/:id', (req, res) =>{ // request & response
   const { id } = req.params;
   const product = service.findOne(id);
   res.json(product)
 });
 
-// 2. Create a new product
+// 3. Create a new product
 router.post('/', (req,res) => {
   const body = req.body;
   const newProduct = service.create(body);
   res.json(newProduct);
 });
 
-// 3. Update partially a product
+// 4. Update partially a product
 router.patch('/:id', (req,res) => {
   const { id } = req.params;
   const body = req.body;
@@ -36,7 +35,7 @@ router.patch('/:id', (req,res) => {
   res.json(product)
 });
 
-// 3. Update partially a product
+// 5. Update partially a product
 router.put('/:id', (req,res) => {
   const { id } = req.params;
   const body = req.body;
@@ -44,8 +43,7 @@ router.put('/:id', (req,res) => {
   res.json(product)
 });
 
-
-// 4. Delete  a product
+// 6. Delete  a product
 router.delete('/:id', (req,res) => {
   const { id } = req.params;
   const product = service.delete(id);

0cfc9fb1fdb551cf99a82a4a19debf3b00c56f19
BioDani <dtejadah@unal.edu.co>
2023-04-10 00:36:15 -0500

    detalles de puerto

--- API_rest_express/index.js
+++ API_rest_express/index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes');
 const {logErrors,errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 
 const app = express(); // Create application
-const PORT = 3000; // PORT where it will run
+const PORT = process.env.PORT || 8000; // PORT where it will run
 
 app.use(express.json()); // Use of a middleware to receive data in json format
 

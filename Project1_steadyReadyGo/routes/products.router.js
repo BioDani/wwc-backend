f5c93b714864c705fff881c2c06435d8cc3b55c2
BioDani <dtejadah@unal.edu.co>
2023-04-11 11:59:14 -0500

    CRUD working - without services

--- Project1_steadyReadyGo/routes/products.router.js
+++ Project1_steadyReadyGo/routes/products.router.js
@@ -2,21 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/',
-async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // request & response
   try {
-    const products =   {
-      id : 1,
-      title:"macbook pro ",
-      description:"bla bla bla bla bla",
-      price:2000,
-      discountPercentage:0,
-      rating:3.3,
-      stock:1,
-      brand:"apple",
-      category : "laptop",
-      images: ["https://loremflickr.com/640/480", "https://loremflickr.com/640/481"]
+    const products = {
+      id: 1,
+      title: 'macbook pro ',
+      description: 'bla bla bla bla bla',
+      price: 2000,
+      discountPercentage: 0,
+      rating: 3.3,
+      stock: 1,
+      brand: 'apple',
+      category: 'laptop',
+      images: [
+        'https://loremflickr.com/640/480',
+        'https://loremflickr.com/640/481',
+      ],
     };
     res.json(products);
   } catch (error) {
@@ -24,14 +26,53 @@ async (req, res, next) => {
   }
 });
 
-router.post('/',
-async(req,res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const body = req.body;
     res.json({
-      message : "created",
-      data : body
-    })
+      message: 'created',
+      data: body,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:id', async(req, res, next)=>{
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({
+      message: 'updated',
+      data: body,
+      id
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/:id', async(req, res, next)=>{
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({
+      message: 'updated',
+      data: body,
+      id
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async(req, res, next)=>{
+  try {
+    const { id } = req.params;
+    res.json({
+      message: 'deleted',
+      id
+    });
   } catch (error) {
     next(error);
   }
